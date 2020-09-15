71d675e7836f9a41fab59cb82c10d1bfb466c249
Munnazzah Aslam <aslammunnazzah@gmail.com>
2020-09-15 12:34:24 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const button=document.querySelector('.talk');
+const content=document.querySelector('.content');
+const SpeechRecognition= window.SpeechRecognition || window.webkitSpeechRecognition;
+const Recognition= new SpeechRecognition();
+Recognition.onstart=function(){
+    console.log('Voice Recognition is activated, you can speak now');
+};
+
+const greetings=['Doing good you little piece of love','I am good','Alhamdulillah'];
+const madiha=['I like madiha','Madiha is fool','Madiha is studying maths'];
+Recognition.onresult=function(e){
+    const current= e.resultIndex;
+    const transcript=e.results[current][0].transcript;
+    content.textContent=transcript;
+    readoutLoud(transcript);
+};
+
+button.addEventListener('click',()=>{
+    Recognition.start();
+});
+
+function readoutLoud(message){
+    const speech=new SpeechSynthesisUtterance();
+    speech.text='I dont know what are you saying, please repeat!';
+
+    if(message.includes('how are you')){
+        const finalmessage= greetings[Math.floor(Math.random()* greetings.length)];
+        speech.text=finalmessage;
+    } 
+    if(message.includes('Who is madiha')){
+        const finalmessage= greetings[Math.floor(Math.random()* greetings.length)];
+        speech.text=finalmessage;
+    } 
+    speech.volume=1;
+    speech.pitch=1;
+    speech.rate=1;
+    window.speechSynthesis.speak(speech);
+}
